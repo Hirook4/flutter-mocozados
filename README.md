6256f5426c074475a516b241ba127b48c71dcff7
Leonardo Hirooka <leonardohirooka@hotmail.com>
2025-10-04 17:25:58 -0300

    remove all previous files and prepare for new version

--- README.md
+++
@@ -1,3 +0,0 @@
-# mocozados
-
-flutter + firebase + google maps 
--- android/app/src/main/kotlin/com/example/mocados/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mocozados
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
